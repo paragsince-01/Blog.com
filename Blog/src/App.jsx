3690da46fb87f2110bb8e5ff796d8712dbfdc1dc
Paragsince-01 <aggarwalparag5@gmail.com>
2024-02-09 19:24:49 +0530

    Routes are created

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
+import Home from './Componets/Home'
+import About from './Componets/About'
+import Projects from './Componets/Projects'
+import Signin from './Componets/Signin'
+import Signup from './Componets/Signup'
+import Dashboard from './Componets/Dashboard'
 
 export default function App() {
   return (
     <>
-      <h1 className="text-xl font-bold underline">Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/About" element={<About/>}/>
+          <Route path="/Projects" element={<Projects />}/>
+          <Route path="/Signin" element={<Signin/>}/>
+          <Route path="/Signup" element={<Signup/>}/>
+          <Route path="/Dasboard" element={<Dashboard/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
