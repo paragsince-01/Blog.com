570586ed75c8707f44d48a739ace15a3cea3d27f
Paragsince-01 <aggarwalparag5@gmail.com>
2024-02-10 15:00:00 +0530

    server is connected to the mongodb database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('MongoDB is Connected');
+}).catch((err)=>{
+    console.log(err);
+});
 const app = express();
 
 app.listen(
