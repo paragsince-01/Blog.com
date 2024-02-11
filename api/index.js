ba76898d85af0669e1d911f906bda20279b5ebe5
Paragsince-01 <aggarwalparag5@gmail.com>
2024-02-11 10:57:46 +0530

    signup api route is created

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js'
+import authRoutes from './routes/authRoute.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO)
     console.log(err);
 });
 const app = express();
+app.use(express.json());
 
 app.listen(
     3000, ()=>{
@@ -18,4 +20,5 @@ app.listen(
     }
 );
 
-app.use('/api/user',userRoutes)
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
