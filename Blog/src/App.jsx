a04e2ad46bcded68ee41001dbcbdd8188e3ae9e6
Paragsince-01 <aggarwalparag5@gmail.com>
2024-02-15 13:47:55 +0530

    now dashboarad is private only for the signed in user

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -8,6 +8,7 @@ import Signup from './Pages/Signup'
 import Dashboard from './Pages/Dashboard'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import PrivateRoute from './Components/PrivateRoute'
 
 export default function App() {
   return (
@@ -20,7 +21,9 @@ export default function App() {
           <Route path="/Projects" element={<Projects />}/>
           <Route path="/Signin" element={<Signin/>}/>
           <Route path="/Signup" element={<Signup/>}/>
-          <Route path="/Dasboard" element={<Dashboard/>}/>
+          <Route element={<PrivateRoute />}>
+             <Route path="/Dashboard" element={<Dashboard/>}/>
+          </Route>          
         </Routes>
         <Footer />
       </Router>
