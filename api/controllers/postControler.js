97ff2122ff06e48ec09d8572cb2eafda0465926d
Paragsince-01 <aggarwalparag5@gmail.com>
2024-02-18 15:17:31 +0530

    create a post api route is created

---
+++ api/controllers/postControler.js
@@ -0,0 +1,28 @@
+import Post from "../models/postModel.js";
+import { errorHandler } from "../utils/error.js";
+
+export const create = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You Are Not Allowed To Create A Post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please Fill All The Fields"));
+  }
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "-");
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
