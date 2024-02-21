76b1d07492ccb47cee058619d25bcd204d374aa7
Paragsince-01 <aggarwalparag5@gmail.com>
2024-02-21 12:08:23 +0530

    get the users api route is created

--- api/controllers/userControl.js
+++ api/controllers/userControl.js
@@ -9,10 +9,9 @@ export const test = (req, res) => {
 // update the user
 
 export const updateUser = async (req, res, next) => {
-
-if (req.user.id !== req.params.userId) {
-  return next(errorHandler(403, 'YOU ARE NOT ALLOWED TO UPDATE THIS USER'));
-}
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "YOU ARE NOT ALLOWED TO UPDATE THIS USER"));
+  }
 
   if (req.body.password) {
     if (req.body.password.length < 6) {
@@ -59,25 +58,66 @@ if (req.user.id !== req.params.userId) {
 
 // Delete the user
 
-export const deleteUser = async (req, res, next) =>{
-
-if (req.user.id !== req.params.userId) {
-  return next(errorHandler(403, 'YOU ARE NOT ALLOWED TO DELETE THIS USER'));
-}
-try {
-  await User.findByIdAndDelete(req.params.userId);
-  res.status(200).json('USER HAS BEEN DELETED SUCCESSFULLY');
-} catch (error) {
-  
-}
-}; 
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "YOU ARE NOT ALLOWED TO DELETE THIS USER"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("USER HAS BEEN DELETED SUCCESSFULLY");
+  } catch (error) {}
+};
 
 // signed out the user
 
-export const signout = (req, res, next)=>{
+export const signout = (req, res, next) => {
   try {
-    res.clearCookie('access_token').status(200).json('USER HAS BEEN SIGNEDOUT')
+    res.clearCookie("access_token").status(200).json("USER HAS BEEN SIGNEDOUT");
   } catch (error) {
     next(error);
   }
-}
+};
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "YOU ARE NOT ALLOWED TO SEE THE USERS"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassowrd = users.map((user)=>{
+      const {password, ...rest} = user._doc;
+      return rest;
+    });
+
+      
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: usersWithoutPassowrd,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, updateUser, deleteUser, signout }  from "../controllers/userControl.js";
+import { test, updateUser, deleteUser, signout, getUsers }  from "../controllers/userControl.js";
 import {verifyToken} from '../utils/verifyUser.js'
 
 const router = express.Router();
@@ -11,5 +11,7 @@ router.put("/update/:userId", verifyToken,updateUser);
 router.delete("/delete/:userId", verifyToken,deleteUser);
 
 router.post("/signout", signout);
+
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
